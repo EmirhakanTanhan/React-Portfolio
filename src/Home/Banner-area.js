3f77099972f126e0126ef876349921035115f601
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-06-07 17:41:03 +0300

    imp

--- src/Home/Banner-area.js
+++ src/Home/Banner-area.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import svg_proud from '../Assets/SVG/feeling_proud.svg';
 import svg_wandering from '../Assets/SVG/wandering.svg';
+import svg_personalization from '../Assets/SVG/personalization.svg';
+import svg_in_office from '../Assets/SVG/on_the_office.svg';
+import svg_quitting_time from '../Assets/SVG/quitting_time.svg';
+import svg_work_from_anywhere from '../Assets/SVG/working_from_anywhere.svg';
 
 
 const BannerArea = () => {
@@ -13,7 +17,7 @@ const BannerArea = () => {
             </h1>
 
             <div className="banner-img">
-                <img draggable="false" src={svg_proud} alt="Emirhakan Tanhan (Casually sitting)"/>
+                <img draggable="false" src={svg_work_from_anywhere} alt="Emirhakan Tanhan (Casually sitting)"/>
             </div>
 
         </div>
--- src/Home/blog-area.js
+++ src/Home/blog-area.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
+import img_1 from '../Assets/IMG/verification_via_email.jpg';
+import {Link} from "react-router-dom";
 
 const BlogArea = () => {
+    const [blog, setBlog] = useState([
+        {
+            pills: ['Beginner', 'PHP', 'MySQL'],
+            title: 'Figuring out a verification system via email link',
+            date: 'May 31, 2020',
+            link: 'https://dev.to/emirhakantanhan/figuring-out-a-verification-system-via-email-link-59p1'
+        }
+    ])
+
     return (
         <div className="blog-area">
+            <div className="title">
+                <h2>Articles <i className="fas fa-feather"></i></h2>
+                <p>Once in a while, I write blog articles . Here are the most recent onces:</p>
+            </div>
 
+            <div className="blogs">
+
+                {blog.map((blog) => (
+                    <div className="blog">
+                        <a target="_blank"
+                           href={blog.link}>
+                            <div className="img">
+                                <img src={img_1} alt="verification_via_email"/>
+                            </div>
+                            <div className="chin">
+                                <div className="pills">
+                                    {blog.pills.map((pill) => (
+                                        <span>{ pill }</span>
+                                    ))}
+                                </div>
+                                <div className="title">
+                                    <p>{ blog.title }</p>
+                                </div>
+                                <div className="date">
+                                    <span>{ blog.date }</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                ))}
+
+            </div>
         </div>
     );
 };
